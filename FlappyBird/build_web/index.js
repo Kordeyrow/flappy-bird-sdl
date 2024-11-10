5bade1bd7c582057a9fb6b5ee504309c5d687353
Matheus Cordeiro <matheusgoncalvescordeiro@gmail.com>
2024-11-10 04:02:14 -0300

    ! fix msg of deleted files from git

--- FlappyBird/build_web/index.js
+++ FlappyBird/build_web/index.js
@@ -35,7 +35,7 @@ if (ENVIRONMENT_IS_NODE) {
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpiqtvlq41.js
+// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmp4rdexfjo.js
 
   Module['expectedDataFileDownloads'] ??= 0;
   Module['expectedDataFileDownloads']++;
@@ -221,21 +221,21 @@ Module['FS_createPath']("/assets", "sprites", true, true);
 
   })();
 
-// end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpiqtvlq41.js
-// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmp485wnshz.js
+// end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmp4rdexfjo.js
+// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpyo25kcfy.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['$ww'] || (typeof ENVIRONMENT_IS_PTHREAD != 'undefined' && ENVIRONMENT_IS_PTHREAD)) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmp485wnshz.js
-// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpy7tgixpl.js
+  // end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpyo25kcfy.js
+// include: C:\Users\KORDEY~1\AppData\Local\Temp\tmps0e5yr2h.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach((task) => {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmpy7tgixpl.js
+  // end include: C:\Users\KORDEY~1\AppData\Local\Temp\tmps0e5yr2h.js
 
 
 // Sometimes an existing Module object exists with properties
